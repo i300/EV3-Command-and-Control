0782e23b20983a1b9e636f8b9702ddb92058027d
Jerry Brown <thejerishbrown@gmail.com>
2016-06-27 13:11:52 -0400

    Added EV3MessengerLib to project

---
+++ EV3CommandAndControl/ScollingWidgetView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EV3CommandAndControl
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ScollingWidgetView : Gtk.Bin
+	{
+		public ScollingWidgetView()
+		{
+			this.Build();
+		}
+	}
+}
+
